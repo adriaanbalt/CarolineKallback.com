2fd4ed4983ca9047136e663d9c09c9000fa19bff
Adriaan Scholvinck <Adriaan@BALT.us>
2014-12-17 16:14:29 -0500

    adding header directive and controller shell

---
+++ app/components/header/controller.js
@@ -0,0 +1,32 @@
+"use strict";
+	
+/**
+* @ngdoc controller
+* @name project.header.controller:HeaderController
+* @description
+* Header header functionality for next, previous functions. initializes the various header slides.
+*
+* @param {Object} $rootScope root scope
+* @param {Object} $scope scope of this element
+* @param {Object} $timeout timeout angular function
+*/   
+angular.module('project')
+     .controller('HeaderController', ['$rootScope', '$scope', '$timeout', function($rootScope, $scope, $timeout) {
+
+     Header = {
+          /**
+          * @ngdoc function
+          * @name init
+          * @methodOf project.header.controller:HeaderController
+          * @description
+          * initialize the header by creating the slides based on the data
+          */
+          init: function() {
+               
+          }
+     }
+
+     // initialize the HERO
+     $timeout( Header.init, 1000 );
+
+}]);
