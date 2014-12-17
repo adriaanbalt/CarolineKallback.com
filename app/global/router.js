cbc898ab184ee6c1365d13bec7a01fef73debde2
Adriaan Scholvinck <Adriaan@BALT.us>
2014-12-17 16:28:28 -0500

    fixing routes

--- app/global/router.js
+++ app/global/router.js
@@ -1,40 +1,16 @@
-angular.module('project', ['ngRoute'])
+angular.module('carolinekallback', ['ngRoute'])
 	.config(function ($compileProvider){
 	    $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
 	})
 	.config(function ($routeProvider, $locationProvider) {
 		$routeProvider
 			.when('/', {
-				templateUrl: 'js/start/view.html',
+				templateUrl: 'js/home/view.html',
 				sectionName: "start"
 			})
-			.when('/start', {
-				templateUrl: 'js/start/view.html',
-				sectionName: "start"
-			})
-			.when('/titles', {
-				templateUrl: 'js/title/view.html',
+			.when('/:id', {
+				templateUrl: 'js/project/view.html',
 				sectionName: "titles"
-			})
-			.when('/title', {
-				templateUrl: 'js/title/view.html',
-				sectionName: "titles"
-			})
-			.when('/record', {
-				templateUrl: 'js/record/view.html',
-				sectionName: "record"
-			})
-			.when('/finish', {
-				templateUrl: 'js/finish/view.html',
-				sectionName: "finish"
-			})
-			.when('/montage', {
-				templateUrl: 'js/montage/view.html',
-				sectionName: "montage"
-			})
-			.when('/preview/:id', {
-				templateUrl: 'js/preview/view.html',
-				sectionName: "preview"
 			})
 			.when('/error', {
 				templateUrl: 'js/error/view.html',
