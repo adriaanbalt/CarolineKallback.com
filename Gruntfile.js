8bfb260d75a79c762e41b2236e8b0f051d2f4e0c
adriaanbalt <adriaan@balt.us>
2017-11-18 12:17:33 -0500

    including the dynamic grunt version

---
+++ Gruntfile.js
@@ -0,0 +1,227 @@
+var paths = {
+	js: ['app/Assets/js/**/*.js'],
+	html: ['app/views/**/*.hbs'],
+	css: ['app/scss/mixins/*.scss', 'app/scss/*.scss'],
+	data: ['app/languages/**/*.json']
+};
+
+module.exports = function(grunt) {
+	'use strict';
+
+	require('time-grunt')(grunt);
+
+	grunt.initConfig({
+
+		// Package Info
+		pkg: grunt.file.readJSON('package.json'),
+
+		// Configurable App Directories
+		directory: {
+			app: 'app',
+			dist: 'dist',
+			root: ''
+		},
+
+		// Grunt Clean
+		// Clear files and folders that are auto generated
+		// https://github.com/gruntjs/grunt-contrib-clean
+		clean: {
+			dist: {
+				files: [{
+					dot: true,
+					src: [
+						'<%= directory.dist %>/*'
+					]
+				}]
+			}
+		},
+
+		// Grunt Copy
+		// Copies files and folders
+		// https://github.com/gruntjs/grunt-contrib-copy
+		copy: {
+			dist: {
+				files: [{
+					expand: true,
+					dot: true,
+					cwd: '<%= directory.app %>',
+					dest: '<%= directory.dist %>',
+					src: [
+						'Assets/css/**/*',
+						'Assets/js/**/*',
+						'Assets/images/**/*',
+						'media/**/*'
+					]
+				}]
+			}
+		},
+
+		// Compass
+		// Generates the CSS from SCSS files
+		// https://github.com/gruntjs/grunt-contrib-compass
+		compass: {
+			dist: {
+				options: {
+					sassDir: '<%= directory.app %>/scss',
+					cssDir: '<%= directory.dist %>/Assets/css',
+					imagesDir: '<%= directory.dist %>/Assets/images',
+					javascriptDir: '<%= directory.dist %>/Assets/js',
+					relativeAssets: false,
+					outputStyle: 'expanded'
+				}
+			}
+		},
+
+		// CSSmin
+		// Minifies css files
+		// https://github.com/gruntjs/grunt-contrib-cssmin
+		cssmin: {
+			combine: {
+				files: {
+					'<%= directory.dist %>/Assets/css/<%= pkg.name %>.min.css': ['<%= directory.dist %>/Assets/css/<%= pkg.name %>.css']
+				}
+			}
+		},
+
+		// CssLint
+		// To ensure there are no parsing errors in the CSS
+		// https://github.com/gruntjs/grunt-contrib-csslint
+		csslint: {
+			options: {
+				csslintrc: '.csslintrc'
+			},
+			src: ['<%= directory.dist %>/Assets/css/**/*/css']
+		},
+
+		// Assemble
+		// Static site generator for Node.js, Grunt.js, and Yeoman (and soon, Gulp).
+		// https://github.com/assemble/assemble
+		assemble: {
+			dist: {
+				options: {
+					assets: '<%= directory.dist %>',
+					partials: ['<%= directory.app %>/views/partials/**/*.hbs'],
+					layout: ['<%= directory.app %>/views/layouts/default.hbs'],
+					data: ['<%= directory.app %>/languages/**/*.json'],
+					production: true,
+					pages: ['<%= directory.app %>/views/pages/**/*.hbs']
+				},
+				files: [{
+					expand: true,
+					src: ['**/*.hbs'],
+					cwd: '<%= directory.app %>/views/pages/',
+					dest: '<%= directory.dist %>'
+				}]
+			}
+		},
+
+		// Grunt Connect
+		// https://github.com/gruntjs/grunt-contrib-connect
+		// Used to create a static web server for dist
+		connect: {
+			options: {
+				port: 7001,
+				keepalive: true,
+				hostname: 'localhost'
+			},
+			dist: {
+				options: {
+					open: true,
+					base: 'dist'
+				}
+			}
+		},
+
+		// Watch
+		// Watches for changes to specific files
+		// https://github.com/gruntjs/grunt-contrib-watch
+		watch: {
+			js: {
+				files: paths.js,
+				tasks: ['clean:dist'],
+				options: {
+					livereload: true
+				}
+			},
+			html: {
+				files: paths.html,
+				tasks: ['assemble:dist'],
+				options: {
+					livereload: true
+				}
+			},
+			styles: {
+				files: paths.css,
+				tasks: ['compass'],
+				options: {
+					livereload: true
+				}
+			},
+			json: {
+				files: paths.data,
+				tasks: ['assemble:dist'],
+				options: {
+					livereload: true
+				}
+			}
+		},
+
+		// Concurrent
+		// Allow multiple tasks to occur at once
+		// https://github.com/sindresorhus/grunt-concurrent
+		concurrent: {
+			dist: ['watch','connect:dist'],
+			options: {
+				logConcurrentOutput: true
+			}
+		}
+
+	});
+
+	require('load-grunt-tasks')(grunt,{
+		pattern: ['grunt-*', 'assemble']
+	});
+
+	// Development grunt task
+	grunt.registerTask('build', [
+		// Cleanup Previously Generated Files
+		'clean:dist',
+
+		// Sass compilation
+		'compass:dist',
+
+		// Copy HTML and assets
+		'copy:dist',
+
+		// Ensure no CSS errors
+		'csslint'
+	]);
+
+	// Development grunt task
+	grunt.registerTask('dev', [
+
+		'build',
+
+		// Assemble the HTML files
+		'assemble:dist',
+
+		// Runs both WATCH and CONNECT
+		'concurrent:dist'
+	]);
+
+	// Development grunt task
+	grunt.registerTask('dist', [
+		
+		'build',
+
+		// CSS minification
+		'cssmin',
+
+		// Assemble the HTML files
+		'assemble:dist',
+
+		// Runs both WATCH and CONNECT
+		'concurrent:dist'
+	]);
+
+};
--- README.md
+++ README.md
@@ -1,50 +1,65 @@
 
 # Caroline Kallback website
 
-####Introduction
+###Introduction
 
-Caroline Kallback's online portfolio
-
-#### Node Modules
+### Node Modules
 
 * [Grunt](http://gruntjs.com/)
 * [Grunt-CLI](https://github.com/gruntjs/grunt-cli)
 * [Compass](https://github.com/gruntjs/grunt-contrib-compass)
 * [Concurrent](https://github.com/sindresorhus/grunt-concurrent)
 * [Clean](https://github.com/gruntjs/grunt-contrib-clean)
-* [Concat](https://github.com/gruntjs/grunt-contrib-concat)
 * [Connect](https://github.com/gruntjs/grunt-contrib-connect)
 * [grunt-contrib-copy](https://github.com/gruntjs/grunt-contrib-copy)
 * [grunt-contrib-csslint](https://github.com/gruntjs/grunt-contrib-csslint)
-* [grunt-contrib-cssmin](https://github.com/gruntjs/grunt-contrib-cssmin)
-* [JS Uglify](https://github.com/gruntjs/grunt-contrib-uglify)
 * [Watch / Live reload](https://github.com/gruntjs/grunt-contrib-watch)
-* [grunt-modernizr](https://github.com/Modernizr/grunt-modernizr)
 * [load-grunt-tasks](https://github.com/sindresorhus/load-grunt-tasks)
-* [time-grunt](https://github.com/sindresorhus/time-grunt)
 
-#### Libraries
+### Libraries
 
-* [Normalize](http://necolas.github.io/normalize.css/)
-* [Modernizr](http://modernizr.com/)
-* [Angular](https://angularjs.org/)
 * [Compass](http://compass-style.org/)
 * [NPM](https://www.npmjs.com)
 * [NVM](https://github.com/creationix/nvm)
 * [RVM](https://github.com/creationix/nvm)
 * [Node](http://nodejs.org/)
 
-#### Requirements
+### Requirements
 Make sure your have the following installed with these versions or greater when running the bootstrap
 
 * ```Node v0.10.26```
 * ```Sass v3.3.9```
 * ```Compass >1.0.0.alpha.20```
 
-#### Installation
+### Installation
 
+1. Pull the master version of this Git repository.
 
-#### Tooling Dependencies
+2. Run "grunt dist" in the root folder of Git repository.
+
+3. This will open a localhost using CONNECT to allow you to review the compiled website based on the JSON files located in the "languages" folder.
+
+4. To change language, click the language button in the header to reveal the modal and click the language you would like to view.
+
+5. When ready, upload all files within the "dist" folder.
+
+### File Paths
+
+1. ```in /app/languages/*.json```
+2. find variable "file-path"
+3. change this variable to the path of the language sub-folder
+
+For Example:
+
+* for /nl-nl
+* open /app/languages/nl-nl.json
+* ```"file-path":"http://lp.sonos.com/bluenote/"```
+
+Don't forget:
+
+You must include trailing slash "/" of this variable.
+
+### Tooling Dependencies
 
 ```
 npm install
@@ -52,9 +67,14 @@ npm install
 
 ### Grunt Task(s)
 
-#### Dev
-Runs the DEV task
+### Dist
+Runs the DIST task.  Compiles the languages into their corresponding folders in the /dist directory.
 
 ```
-grunt dev
+grunt dist
 ```
+
+### Uploading to Host
+
+1. Upload the contents of the "/dist" folder.
+
